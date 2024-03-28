ed07a76a6642ee2af5cc24a5b4e452306ee132f1
joyjos <enjoyjos@hotmail.com>
2024-03-28 01:34:33 +0100

    CORS configuration modified

--- backend/src/main/java/org/factoriaf5/backend/configuration/CorsConfiguration.java
+++ backend/src/main/java/org/factoriaf5/backend/configuration/CorsConfiguration.java
@@ -1,27 +1,17 @@
 package org.factoriaf5.backend.configuration;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfiguration {
+public class CorsConfiguration implements WebMvcConfigurer {
 
-    @Value("${app.cors.client-url}")
-    private String[] allowedOrigins;
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry
-                .addMapping("/**")
-                .allowedOrigins(allowedOrigins)
-                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS");
-            }
-        };
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
+                .allowCredentials(true);
     }
 }
