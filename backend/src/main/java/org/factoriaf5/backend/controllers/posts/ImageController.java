d99dd2d909cd85aa53ea59b64feb70f8e84226ab
joyjos <enjoyjos@hotmail.com>
2024-04-10 14:53:07 +0200

    ImageController implemented

---
+++ backend/src/main/java/org/factoriaf5/backend/controllers/posts/ImageController.java
@@ -0,0 +1,41 @@
+package org.factoriaf5.backend.controllers.posts;
+
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.MalformedURLException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@RestController
+@RequestMapping("api/images")
+public class ImageController {
+
+    private final Path uploadDir = Paths.get("uploads"); // Ruta donde se encuentran las imágenes
+
+    @GetMapping("/{imageName:.+}")
+    public ResponseEntity<Resource> getImage(@PathVariable String imageName) {
+        try {
+            Path imagePath = uploadDir.resolve(imageName).normalize();
+            Resource resource = new UrlResource(imagePath.toUri());
+
+            if (resource.exists() && resource.isReadable()) {
+                return ResponseEntity.ok()
+                        .contentType(MediaType.IMAGE_JPEG) // Cambiar el tipo de contenido según el tipo de imagen
+                        .body(resource);
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+        } catch (MalformedURLException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+}
+
